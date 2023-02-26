36f217e0cce3358062d47f6fce6fca9d59e02d1d
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-02-26 17:12:30 +0300

    Task 66

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Task 66");
+Console.WriteLine("-------");
+
+int SumNumbers(int m, int n)
+{
+    if (n == m)
+        return m;
+    return SumNumbers(m, n - 1) + n;
+}
+
+Console.Write("Введите 1-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от {m} до {n}: {SumNumbers(m, n)}");
+
+Console.ReadLine();
+
+/*--------------------------------------------*/
